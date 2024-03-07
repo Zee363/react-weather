12b8d7af3f961444bf59f3ab88dbdb9be5a97a03
Zee363 <133252086+Zee363@users.noreply.github.com>
2024-03-07 17:35:30 +0200

    Removed the header in App.js and Added some CSS

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header>
-        <h1>Weather App</h1>
-      </header>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -2,16 +2,34 @@ import React from "react";
 import "./Weather";
 import "./Weather.css";
 
-export default function Weather () {
+export default function Weather() {
   return (
     <div className="container">
       <form>
-        <input type="text" placeholder="Search for a city.."/>
-        <input type="submit" className="btn btn-primary" />
+        <input type="text" placeholder="Search for a city.." id="search-form-input" />
+        <input type="submit" className="btn btn-primary" id="search-form-button"/>
       </form>
-    <div className="Weather">
-    <h1>Cape Town</h1>
+      <div className="Weather">
+        <h1>Cape Town</h1>
+        <p>
+          <light className="fs-1">
+            <em>29&deg;C</em>&#9728;
+          </light>
+          <br />
+          <hr />
+          <ul>
+          <li>
+          Wed 17:22, Sunny
+          </li>
+          <li>
+          Humidity: 12%
+          </li>
+          <li>
+          Wind: 17km/h
+          </li>
+          </ul>
+        </p>
+        </div>
     </div>
-    </div>
-  )
+  );
 }
