be2c264b4393f6ff9f758644d20f877d0b2a6925
Zee363 <133252086+Zee363@users.noreply.github.com>
2024-03-11 15:34:28 +0200

    Added google fonts and adjusted the CSS accordingly

--- src/Weather.js
+++ src/Weather.js
@@ -6,14 +6,14 @@ export default function Weather() {
   return (
     <div className="container">
       <form>
-        <input type="text" placeholder="Search for a city.." id="search-form-input" />
+        <input type="text" placeholder="Search for a city.." id="search-form-input" autoFocus="on"/>
         <input type="submit" className="btn btn-primary" id="search-form-button"/>
       </form>
       <div className="Weather">
         <h1>Cape Town</h1>
         <p>
           <light className="fs-1">
-            <em>29&deg;C</em>&#9728;
+            <em>29&deg;C</em> &#9728;
           </light>
           <br />
           <hr />
