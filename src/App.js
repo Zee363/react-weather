732699596bdcbf8b6f56d25488648a0ffd220a06
Zee363 <133252086+Zee363@users.noreply.github.com>
2024-03-20 19:14:39 +0200

    Included a path for the  project on Netlify

--- src/App.js
+++ src/App.js
@@ -5,7 +5,31 @@ function App() {
   return (
     <div className="App">
       <Weather defaultCity="New York" />
-      <footer>This project was coded by <a href="https://www.github.com/Zee363" target="_blank" rel="noopener noreferrer">Zinhle Nhlapo</a> and it is <a href="https://www.github.com/Zee363/react-weather" target="_blank" rel="noopener noreferrer">open-sourced on Github.</a> It is also hosted on <a href="" target="_blank" rel="noopener noreferrer">Netlify.</a> 
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.github.com/Zee363"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Zinhle Nhlapo
+        </a>{" "}
+        and it is{" "}
+        <a
+          href="https://www.github.com/Zee363/react-weather"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on Github.
+        </a>{" "}
+        It is also hosted on{" "}
+        <a
+          href="https://voluble-gingersnap-8f2806.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Netlify.
+        </a>
       </footer>
     </div>
   );
