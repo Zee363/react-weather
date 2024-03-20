6a0cb7044daad84b3f13541ddad74e11a763d77c
Zee363 <133252086+Zee363@users.noreply.github.com>
2024-03-20 18:52:28 +0200

    Updated my links

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <Weather defaultCity="New York" />
-      <footer>This project was coded by <a href="https://www.github.com/Zee363" target="_blank" rel="nonopener nonreferrer">Zinhle Nhlapo</a> and it is <a href="https://www.github.com/Zee363/react-weather" target="_blank" rel="nonopener nonreferrer">open-sourced on Github.</a> It is also hosted on <a href="" target="_blank" rel="nonopener nonreferrer">Netlify.</a> 
+      <footer>This project was coded by <a href="https://www.github.com/Zee363" target="_blank" rel="noopener noreferrer">Zinhle Nhlapo</a> and it is <a href="https://www.github.com/Zee363/react-weather" target="_blank" rel="noopener noreferrer">open-sourced on Github.</a> It is also hosted on <a href="" target="_blank" rel="noopener noreferrer">Netlify.</a> 
       </footer>
     </div>
   );
