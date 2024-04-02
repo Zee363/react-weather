8f59847e54990cbcfbb1d87ed45a570b84f6dce6
Zee363 <133252086+Zee363@users.noreply.github.com>
2024-04-02 16:49:01 +0200

    Updated WeatherInfo.js

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -6,29 +6,32 @@ export default function WeatherInfo({ data }) {
   return (
     <div className="WeatherInfo">
       <div className="Weather">
-          <h1>{data.city}</h1>
-          <p className="weather-data-list">
-            <div className="weather-app-temperature-container">
-              <div className="temperature">
-                <strong>{Math.round(data.temperature)}</strong>
-              </div>
-              <div className="unit">
-                &deg;C
-              </div>{" "}
-              <img src={data.iconUrl} alt={data.description} />
-              </div>
-            <ul>
-              <li>
-                <FormattedDate date={data.date} />
-              </li>
-              <li className="text-capitalize">{data.description}
-                </li>
-            <li>Humidity:<strong> {data.humidity}%</strong></li>
-            <li>Wind:<strong>{data.wind}km/h</strong></li>
-            </ul>
-          </p>
-          </div>
-        <hr />
+        <h1>{data.city}</h1>
+        <div className="weather-data-list">
+          <div className="weather-app-temperature-container">
+            <div className="temperature">
+                  <strong>{Math.round(data.temperature)}</strong>
+                  <div className="unit">&deg;C</div>{" "}
+                  <img src={data.iconUrl} alt={data.description} />
+                </div>
+                </div>
+                </div>
+            <p>
+              <ul>
+                  <li>
+                    <FormattedDate date={data.date} />
+                  </li>
+                  <li className="text-capitalize">{data.description}</li>
+                  <li>
+                    Humidity:<strong> {data.humidity}%</strong>
+                  </li>
+                  <li>
+                    Wind:<strong>{data.wind}km/h</strong>
+                  </li>
+              </ul>
+            </p>
       </div>
-    );
+      <hr />
+    </div>
+  );
   }
